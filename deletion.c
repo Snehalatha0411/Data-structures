f7d07a87b75d1163b8c34420400773e3469b634e
Snehalatha0411 <127390547+Snehalatha0411@users.noreply.github.com>
2023-10-13 11:11:26 +0530

    Add files via upload

--- deletion.c
+++ deletion.c
@@ -5,7 +5,7 @@ struct single_linked_list
         int data;
         struct single_linked_list *p;
 };
-void delete_beg(struct single_linked_list *head);
+void delete_end(struct single_linked_list *head);
 int main()
 {
         struct single_linked_list *head=(struct single_linked_list*)malloc(sizeof(struct single_linked_list));
@@ -21,16 +21,16 @@ int main()
         current->data=30;
         current->p=NULL;
         head->p->p=current;
-        delete_beg(head);
+        delete_end(head);
 }
-void delete_beg(struct single_linked_list *head)	
+void delete_end(struct single_linked_list *head)	
 {
 	struct single_linked_list *temp=head;
 	if(head==NULL)
 		printf("List is empty");
 	else
 	{
-		head=temp->p;
+		temp=temp->p->p;
 	printf(" deleted element is %d ",temp->data);
         free(temp);
 	}
