7c23f462958b5c77bcb87bef0c61cac49eb10e60
Snehalatha0411 <127390547+Snehalatha0411@users.noreply.github.com>
2023-10-13 10:35:06 +0530

    Add files via upload

--- traverse.c
+++ traverse.c
@@ -6,53 +6,61 @@ struct single_linked_list
         struct single_linked_list *p;
 };
 void traverse(struct single_linked_list *head);
-void count_of_nodes(struct single_linked_list *head);
+void insert_random(struct single_linked_list *head,int data,int position);
 int main()
 {
-        struct single_linked_list *head=(struct single_linked_list *)malloc(sizeof(struct single_linked_list));
+        struct single_linked_list *head=(struct single_linked_list*)malloc(sizeof(struct single_linked_list));
         head->data=10;
         head->p=NULL;
-        struct single_linked_list *current=(struct single_linked_list *)malloc(sizeof(struct single_linked_list));
+
+        struct single_linked_list *current=(struct single_linked_list*)malloc(sizeof(struct single_linked_list));
         current->data=20;
         current->p=NULL;
         head->p=current;
-        current=(struct single_linked_list *)malloc(sizeof(struct  single_linked_list));
+
+        current=(struct single_linked_list*)malloc(sizeof(struct single_linked_list));
         current->data=30;
         current->p=NULL;
         head->p->p=current;
-        printf("%d %d %d",head->data,head->p->data,head->p->p->data);
+
+        //printf("%d %d %d",head->data,head->p->data,head->p->p->data);
+	printf("Before inserting a random node\n");
+	traverse(head);
+	printf("After inserting a random node\n");
+	insert_random(head,40,3);
 	traverse(head);
 }
+
 void traverse(struct single_linked_list *head)
 {
-        struct single_linked_list *temp=(struct single_linked_list *)malloc(sizeof(struct single_linked_list));
-        temp->p=head;
-        if(temp->p==NULL)
+        struct single_linked_list *temp=head;
+        
+        if(temp==NULL)
         {
         printf("single linked list is empty");
         exit(0);
         }
-        while(temp->p!=NULL)
+
+        while(temp!=NULL)
         {
-        printf("%d",temp->data);
+        printf("%d->",temp->data);
         temp=temp->p;
         }
+	printf("NULL\n");
 }
-void count_of_nodes(struct single_linked_list *head)
+
+void insert_random(struct single_linked_list *head,int data,int position)
 {
-	struct single_linked_list *temp=NULL;
-	temp->p=head;
-	int count=0;
-	if(temp->p==NULL)
+	struct single_linked_list *new=(struct single_linked_list*)malloc(sizeof(struct single_linked_list));
+        new->data=data;
+	new->p=NULL;
+
+	struct single_linked_list *temp=head;
+	while(position!=2)
 	{
-	 	printf("linked list is empty");
+		temp=temp->p;
+	        position--;
 	}
-	while(temp->p!=NULL)
-	{
-	count=count+1;
-	temp=temp->p;
-	printf("%d",count);
-	}
+	new->p=temp->p;
+	temp->p=new;
 }
-
-
